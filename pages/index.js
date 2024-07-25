59ab46ceed562d21fbf0d935a616def4756b8b82
caarolinaoliveira <138406827+caarolinaoliveira@users.noreply.github.com>
2024-07-25 16:48:32 +0000

    feat: prettier finalizado

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1> Bom dia!! você é um ótimo irmão </h1>
+  return <h1> Bom dia!! você é um ótimo irmão </h1>;
 }
-
+function test() {
+  console.log("teste");
+}
+function test() {
+  console.log("teste");
+}
 export default Home;
