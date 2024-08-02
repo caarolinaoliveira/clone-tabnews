43aa4304365e6d50168ff5b62b57f55318e6dc83
caarolinaoliveira <138406827+caarolinaoliveira@users.noreply.github.com>
2024-08-02 17:51:57 +0000

    feat: primeiro endpoint criado

---
+++ pages/api/status.js
@@ -0,0 +1,4 @@
+function status(request, response) {
+  response.status(200).json("alunos do curso.dev são pessoas acima da média");
+}
+export default status;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,4 @@
 function Home() {
   return <h1> Bom dia!! você é um ótimo irmão </h1>;
 }
-function test() {
-  console.log("teste");
-}
-function test() {
-  console.log("teste");
-}
 export default Home;
